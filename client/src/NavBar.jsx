467dd7427e70b690f8a7300119bcad6796b00499
Bryce Butenhof <brycebutenhof@gmail.com>
2024-11-01 15:09:26 -0500

    Fix tabname, add link to barspoon title

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+    const navigate = useNavigate();
 
     return (
         <nav className="bg-gray-500 py-6 sticky top-0">
             <div className="container mx-6 flex justify-between items-center">
-                <h1 className="font-bold">barspoon.io</h1>
+                <h1 className="font-bold cursor-pointer" onClick={() => navigate('/')}>barspoon.io</h1>
             </div>
         </nav>
     );
